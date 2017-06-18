f1718e569328fbfefdcf4dc2cbfd82108e7c83c5
emestabillo <emsestabillo@gmail.com>
2017-06-18 15:46:37 -0400

    Database setup

---
+++ db/migrate/20170618193606_create_weathers.rb
@@ -0,0 +1,11 @@
+class CreateWeathers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :weathers do |t|
+      t.string :activity
+      t.string :location
+      t.integer :temperature
+      t.string :condition
+      t.timestamps
+    end
+  end
+end
